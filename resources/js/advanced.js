7b145fce351acfb7d6330ac04eb1cf700bcd34d6
shashishashank6 <shashi.shashank6@gmail.com>
2017-02-27 23:11:16 -0800

    updated animations

---
+++ resources/js/advanced.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+$(function(){
+        $(".js-hero-wp").typed({
+        strings: ["GOODBYE JUNK FOOD.<br /> HELLO SUPER HEALTHY MEALS."],
+        typeSpeed: 0
+      });
+    });
+});
